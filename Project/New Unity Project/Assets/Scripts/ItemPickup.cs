af98df079527db80373f6c8efa86ec87e022b0c1
Scweam Racc <150622426+joao23194@users.noreply.github.com>
2024-05-27 10:16:01 +0100

    Little Changes

--- Project/New Unity Project/Assets/Scripts/ItemPickup.cs
+++ Project/New Unity Project/Assets/Scripts/ItemPickup.cs
@@ -21,11 +21,12 @@ public class ItemPickup : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) ;
+        if (other.CompareTag("Player"))
         {
             Pickup();
             Debug.Log("Item picked up!");
             audioManager.PlaySFX(audioManager.BrickTouch);
         }
+         if (other.CompareTag("NPC")){}
     }
 }
